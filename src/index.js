978fff655e865fd30d290f1976cf302ba3c338c8
Thomas Jenkins <ralf@Ralfs-MBP.home>
2015-09-17 22:41:09 -0400

    Change to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { validateReducer } from './validateReducer'
 export { validateActionName } from './validateActionName'
 export { validateAction } from './validateAction'
+export createAction from './createAction'
