1c872115c1720f1d3251aea42fcaabc5a1db0b09
Thomas Jenkins <ralf@Ralfs-MBP.home>
2015-09-17 20:57:21 -0400

    Added createAction to dist

---
+++ dist/createAction.js
@@ -0,0 +1,44 @@
+'use strict';
+
+exports.__esModule = true;
+exports['default'] = createAction;
+
+function createAction(actionName, payload) {
+  if (typeof payload === 'undefined' || payload === null || arguments.length == 1) {
+    return function () {
+      return {
+        name: actionName
+      };
+    };
+  }
+  if (typeof payload === 'string') {
+    return function (value) {
+      var _data;
+
+      return {
+        name: actionName,
+        data: (_data = {}, _data[payload] = value, _data)
+      };
+    };
+  }
+  if (typeof payload === 'object') {
+    return function (value) {
+      return {
+        name: actionName,
+        data: value
+      };
+    };
+  }
+  if (typeof payload === 'function') {
+    return function () {
+      return {
+        name: actionName,
+        data: payload.apply(undefined, arguments)
+      };
+    };
+  }
+  throw 'Invalid call to createAction, payload needs to be string or function';
+}
+
+module.exports = exports['default'];
+//# sourceMappingURL=createAction.js.map
